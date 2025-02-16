3615ce7e70dbc0858aaa5687143b9d26b99d25c8
Joao Araujo <joaosantos162003@gmail.com>
2025-02-16 18:00:55 -0300

    adiciona funcionalidades de gerenciamento de tickets, incluindo visualização, compra e histórico do usuário

--- controllers/ticketsController.js
+++ controllers/ticketsController.js
@@ -1,172 +1,261 @@
-const Ticket = require('../models/Ticket');
-const TicketStock = require('../models/TicketStock');
-const UserTicket = require('../models/UserTickets');
+const Ticket = require("../models/Ticket");
+const TicketStock = require("../models/TicketStock");
+const UserTicket = require("../models/UserTickets");
 
-// Função para retornar todos os tickets com paginação
 const getTickets = async (req, res) => {
-    const { page = 1 } = req.query;
-    const limit = parseInt(req.query.limit, 10) || 5;
+  const { page = 1 } = req.query;
+  const limit = parseInt(req.query.limit, 10) || 5;
 
-    try {
-        const { count, rows } = await Ticket.findAndCountAll({
-            limit: limit,
-            offset: (page - 1) * limit,
-        });
+  try {
+    const { count, rows } = await Ticket.findAndCountAll({
+      limit: limit,
+      offset: (page - 1) * limit,
+    });
 
-        const lastPage = Math.ceil(count / limit);
-        res.status(200).json({
-            items: rows,
-            currentPage: page,
-            lastPage: lastPage,
-            totalItems: count,
-        });
-    } 
-    
-    catch (error) {
-        res.status(500).json({ message: 'Erro ao recuperar os tickets' });
-    }
+    const lastPage = Math.ceil(count / limit);
+
+    res.render("tickets", {
+      tickets: rows,
+      currentPage: page,
+      lastPage: lastPage,
+      totalItems: count,
+      isAdm: req.user.isAdm,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao recuperar os tickets" });
+  }
 };
 
-// Função que retorna informações de um ticket pelo nome
 const getTicketByName = async (req, res) => {
-    const ticketName = req.params.name;
+  const ticketName = req.params.name;
 
-    try {
-        const ticket = await Ticket.findOne({
-            where: { name: ticketName },
-        });
+  try {
+    const ticket = await Ticket.findOne({
+      where: { name: ticketName },
+    });
 
-        if (!ticket) {
-            return res.status(404).json({ message: "Ticket não encontrado" });
-        }
-
-        res.status(200).json(ticket);
-    } 
-    
-    catch (error) {
-        res.status(500).json({ message: 'Erro ao buscar o ticket' });
+    if (!ticket) {
+      return res.status(404).json({ message: "Ticket não encontrado" });
     }
+
+    res.render("tickets", { tickets: ticket });
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao buscar o ticket" });
+  }
 };
 
-// Função que retorna tickets pelo preço
 const getTicketsByPrice = async (req, res) => {
-    const { page = 1 } = req.query;
-    const limit = parseInt(req.query.limit, 10) || 5;
-    const ticketPrice = parseFloat(req.params.price);
+  const { page = 1 } = req.query;
+  const limit = parseInt(req.query.limit, 10) || 5;
+  const ticketPrice = parseFloat(req.params.price);
 
-    if (isNaN(ticketPrice)) {
-        return res.status(400).json({ message: "Preço inválido" });
+  if (isNaN(ticketPrice)) {
+    return res.status(400).json({ message: "Preço inválido" });
+  }
+
+  try {
+    const { count, rows } = await Ticket.findAndCountAll({
+      where: { price: ticketPrice },
+      limit: limit,
+      offset: (page - 1) * limit,
+    });
+
+    const lastPage = Math.ceil(count / limit);
+
+    if (rows.length === 0) {
+      return res
+        .status(404)
+        .json({
+          message: "Nenhum ticket encontrado para o preço especificado",
+        });
     }
 
-    try {
-        const { count, rows } = await Ticket.findAndCountAll({
-            where: { price: ticketPrice },
-            limit: limit,
-            offset: (page - 1) * limit,
-        });
-
-        const lastPage = Math.ceil(count / limit);
-
-        if (rows.length === 0) {
-            return res.status(404).json({ message: "Nenhum ticket encontrado para o preço especificado" });
-        }
-
-        res.status(200).json({
-            items: rows,
-            currentPage: page,
-            lastPage: lastPage,
-            totalItems: count,
-        });
-    } 
-    
-    catch (error) {
-        res.status(500).json({ message: 'Erro ao buscar tickets' });
-    }
+    res.status(200).json({
+      tickets: rows,
+      currentPage: page,
+      lastPage: lastPage,
+      totalItems: count,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao buscar tickets" });
+  }
 };
 
-// Função para registrar um novo ticket
 const registerTicket = async (req, res) => {
-    const { name, price, type, description } = req.body;
+  const { name, price, quantity } = req.body;
 
-    if (!name || !type || typeof price !== 'number' || !description) {
-        return res.status(400).json({ message: 'Todos os campos são obrigatórios ou os tipos de parâmetros estão incorretos' });
-    }
+  if (!name || !price || !quantity) {
+    return res
+      .status(400)
+      .json({
+        message:
+          "Todos os campos são obrigatórios ou os tipos de parâmetros estão incorretos",
+      });
+  }
 
-    try {
-        const newTicket = await Ticket.create({
-            name,
-            price,
-            type,
-            description
-        });
+  try {
+    const newTicket = await Ticket.create({
+      name,
+      price,
+    });
 
-        res.status(200).json(newTicket);
-    } 
-    
-    catch (error) {
-        res.status(500).json({ message: 'Erro ao criar o ticket' });
-    }
+    await TicketStock.create({
+      quantity,
+      ticketId: newTicket.id,
+    });
+
+    res.redirect("/tickets");
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao criar o ticket" });
+  }
 };
 
 const buyTicket = async (req, res) => {
+  const { ticketId, quantity } = req.body;
+  const userId = req.user.id;
 
-}
+  if (!ticketId || !quantity || quantity <= 0) {
+    return res
+      .status(400)
+      .json({ message: "ID do ticket e quantidade válida são obrigatórios" });
+  }
 
-// Função para atualizar informações de um ticket
-const updateTicket = async (req, res) => {
-    const ticketId = parseInt(req.params.id);
+  try {
+    const ticket = await Ticket.findByPk(ticketId);
 
-    try {
-        const ticket = await Ticket.findByPk(ticketId);
-
-        if (!ticket) {
-            return res.status(404).json({ message: "Ticket não encontrado" });
-        }
-
-        const { name, type, price, description } = req.body;
-
-        if (name) ticket.name = name;
-        if (type) ticket.type = type;
-        if (price) ticket.price = price;
-        if (description) ticket.description = description;
-
-        await ticket.save();
-
-        res.status(200).json(ticket);
-    } 
-    
-    catch (error) {
-        res.status(500).json({ message: 'Erro ao atualizar o ticket' });
+    if (!ticket) {
+      return res.status(404).json({ message: "Ticket não encontrado" });
     }
+
+    const ticketStock = await TicketStock.findOne({
+      where: { ticketId: ticket.id },
+    });
+
+    if (!ticketStock || ticketStock.quantity < quantity) {
+      return res.status(400).json({ message: "Estoque insuficiente" });
+    }
+
+    const userTickets = [];
+    for (let i = 0; i < quantity; i++) {
+      userTickets.push({
+        userId,
+        ticketId,
+      });
+    }
+
+    await UserTicket.bulkCreate(userTickets);
+
+    ticketStock.quantity -= quantity;
+    await ticketStock.save();
+
+    res.status(200).json({
+      message: "Compra realizada com sucesso",
+      ticketsPurchased: quantity,
+    });
+  } catch (error) {
+    console.error("Erro ao comprar tickets:", error);
+    res.status(500).json({ message: "Erro ao comprar tickets" });
+  }
 };
 
-// Função para deletar um ticket
-const deleteTicket = async (req, res) => {
-    const ticketId = parseInt(req.params.id);
+const updateTicket = async (req, res) => {
+  const ticketId = parseInt(req.body.id);
 
-    try {
-        const ticket = await Ticket.findByPk(ticketId);
+  try {
+    const ticket = await Ticket.findByPk(ticketId);
 
-        if (!ticket) {
-            return res.status(404).json({ message: 'Ticket não encontrado.' });
-        }
-
-        await ticket.destroy();
-
-        res.status(200).json(ticket);
-    } 
-    
-    catch (error) {
-        res.status(500).json({ message: 'Erro ao deletar o ticket' });
+    if (!ticket) {
+      return res.status(404).json({ message: "Ticket não encontrado" });
     }
+
+    const { name, price } = req.body;
+
+    if (name) ticket.name = name;
+    if (price) ticket.price = price;
+
+    await ticket.save();
+
+    res.redirect("/tickets");
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao atualizar o ticket" });
+  }
+};
+
+const deleteTicket = async (req, res) => {
+  const ticketId = parseInt(req.params.id);
+
+  try {
+    const ticket = await Ticket.findByPk(ticketId);
+
+    if (!ticket) {
+      return res.status(404).json({ message: "Ticket não encontrado." });
+    }
+
+    await ticket.destroy();
+
+    res.status(200).json(ticket);
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao deletar o ticket" });
+  }
+};
+
+const getUserTickets = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const userTickets = await UserTicket.findAll({
+      where: { userId },
+      include: {
+        model: Ticket,
+        attributes: ["name", "price"],
+      },
+    });
+
+    const data = {
+      userTickets: userTickets.map((userTicket) => ({
+        id: userTicket.id,
+        name: userTicket.Ticket.name,
+        price: userTicket.Ticket.price,
+      })),
+    };
+
+    res.render("userTickets", data);
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao buscar os tickets do usuário" });
+  }
+};
+
+const getHistory = async (req, res) => {
+  try {
+    const userTickets = await UserTicket.findAll({
+      include: {
+        model: Ticket,
+        attributes: ["name", "price"],
+      },
+    });
+
+    const data = {
+      userTickets: userTickets.map((userTicket) => ({
+        id: userTicket.id,
+        name: userTicket.Ticket.name,
+        price: userTicket.Ticket.price,
+      })),
+    };
+
+    res.render("history", data);
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao buscar os tickets do usuário" });
+  }
 };
 
 module.exports = {
-    getTickets,
-    getTicketByName,
-    getTicketsByPrice,
-    buyTicket,
-    registerTicket,
-    updateTicket,
-    deleteTicket
+  getTickets,
+  getTicketByName,
+  getTicketsByPrice,
+  buyTicket,
+  registerTicket,
+  updateTicket,
+  deleteTicket,
+  getUserTickets,
+  getHistory,
 };
